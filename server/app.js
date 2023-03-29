db3cba6a9e4738ab85a9ff54502f70998e2808c0
totskaiasonia@gmail.com <totskaiasonia@gmail.com>
2023-03-29 12:16:30 +0200

    Added connection to DB on sever

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
-const express = require("express")
-const app = express()
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose
+.connect("mongodb+srv://totskaiasonia:gc66WkdNxoZ4Nl9A@cluster0.wupwzix.mongodb.net/?retryWrites=true&w=majority")
+.then(() => console.log("DB connected"))
+.catch((err) => console.log("DB error", err));
+
+const app = express();
 
 app.listen(3000, ()=>{
     console.log('server is listening on port 3000');
