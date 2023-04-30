f71eaba3324672ad024e6c1df6c4b2b0ceab285f
totskaiasonia@gmail.com <totskaiasonia@gmail.com>
2023-04-30 19:49:05 +0200

    Added some fields to WordSchema

--- server/models/Word.js
+++ server/models/Word.js
@@ -3,10 +3,15 @@ import mongoose from 'mongoose';
 const WordSchema = mongoose.Schema(
     {
         word: String,
+        level: String,
         translation: {
             type: Map,
             of: String,
-        }
+        },
+        partOfSpeech: String,
+        typeof: Array,
+        pronunciation: String,
+        frequency: Number
     },
     {
         timestamp: true,
