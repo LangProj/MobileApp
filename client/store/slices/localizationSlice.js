4eb11fb9b785a9bb1233d5727f16b3f86596363d
totskaiasonia@gmail.com <totskaiasonia@gmail.com>
2023-05-05 17:02:58 +0200

    Added - fetching locale file from server - loading locale file from user device - using default file if necessary file not found

--- client/store/slices/localizationSlice.js
+++ client/store/slices/localizationSlice.js
@@ -1,15 +1,68 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from '../../axios.js';
+import * as FileSystem from 'expo-file-system';
 
 export const fetchLocale = createAsyncThunk("localization/fetchLocale", async (loc) => {
-    const response = await fetch(`http://192.168.0.12:3000/localization/${loc}`, { method: "GET" });
-    const data = await response.json();
-    return data;
+    // 1 - проверить есть ли файл с нужной локалью
+    // 2 - если нет то попробовать загрузить с сервера
+    // 3 - если не вышло то загрузить стандартную локаль, которая хранится на устр пользователя и создается при загрузке приложения через установочный пакет приложения
+
+    const path = `${FileSystem.documentDirectory}/lang/${loc}.json`;
+    await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}/lang/`, { intermediates: true });
+
+    try {
+        console.log("Fetching from client files...");
+        let res = await FileSystem.readAsStringAsync(path, {
+            encoding: FileSystem.EncodingType.UTF8
+        })
+        .then((data) => {
+            console.log("Loaded from locale file", data);
+            return JSON.parse(data);
+        })
+        .catch(async (error) => {
+            console.log("Fetching from server...");
+            const {data, status} = await axios.get(`/localization/${loc}`, {
+                validateStatus: function (status) {
+                    return status >= 200 && status < 300;
+                },
+                timeout: 3000
+            })
+                .then(async (response) => {
+                    const dataStr = JSON.stringify(response.data);
+                    await FileSystem.writeAsStringAsync(path, dataStr, {
+                        encoding: FileSystem.EncodingType.UTF8
+                    })
+                        .then(() => console.log("Created new locale file"))
+                        .catch((err) => console.log("Failed to create new locale file:", err));
+                    return response;
+                })
+                .catch(async (error) => {
+                    console.log("Fetching default locale...");
+                    const defaultPath = `${FileSystem.documentDirectory}/lang/default.json`;
+                    const data = await FileSystem.readAsStringAsync(defaultPath, {
+                        encoding: FileSystem.EncodingType.UTF8
+                    })
+                        .then(res => {
+                            console.log("Successfully loaded default file", res);
+                            return res;
+                        })
+                        .catch(err => {
+                            console.log("Error while loading default file", err);
+                        })
+                    return {data: JSON.parse(data)};
+                });
+            return data;
+        });
+        return res;
+    } catch (error) {
+        console.log("Unknown error", error);
+    }
 });
 
 export const localizationSlice = createSlice({
     name: 'localization',
     initialState: {
-        locale: 'en',
+        locale: 'en_US',
         data: {},
         status: "loading",
     },
